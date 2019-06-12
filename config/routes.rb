d2feecb407463acbab5796b37363d63a6dec270a
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-12 07:30:20 +0000

    [Add:] config/routes.rb
    ルートパスを記述、投稿用のルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
+
+  root 'posts#index'
+  resources :posts, only: [:new,:create,:index,:show,:destroy]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
