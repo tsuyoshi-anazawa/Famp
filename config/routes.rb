06c12a73511aec608fcc7bb3d6374daa72f55e68
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-13 08:54:16 +0000

    [Add:] My page
    マイページ機能のためのコントローラとviewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :posts, only: [:new,:create,:index,:show,:destroy]
+  resources :users, only: [:show,:edit,:update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
