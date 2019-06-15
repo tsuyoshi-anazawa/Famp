1e5751a55a79778256a9924d2565d77f0c29e64d
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-15 18:26:28 +0000

    [Fix:] routing
    devise_forのルーティングを一番上に記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'posts#index'
+  devise_for :users
   resources :posts, only: [:new,:create,:index,:show,:destroy]
   resources :users, only: [:show,:edit,:update]
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
