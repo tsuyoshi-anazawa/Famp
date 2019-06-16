bdc482cabf46c653082d7f2e5c197a03cf4b0ead
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-16 13:19:44 +0000

    [Add:] comment
    コメント投稿機能を実装

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,8 +1,19 @@
 class PostCommentsController < ApplicationController
 
 	def create
+		post = Post.find(params[:post_id])
+        comment = PostComment.new(post_comment_params)
+        comment.user_id = current_user.id
+        comment.post_id = post.id
+        comment.save
+        redirect_to post_path(post.id)
 	end
 
 	def destroy
 	end
+
+  private
+    def post_comment_params
+    	params.require(:post_comment).permit(:user_id,:post_id,:comment)
+    end
 end
