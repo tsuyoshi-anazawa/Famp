0f0257bca40a585bbb8f45d3e4262f2c5274b6e3
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-16 08:44:28 +0000

    [Add:] post_comment
    コメントコントローラを作成 コメント機能のルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   devise_for :users
-  resources :posts, only: [:new,:create,:index,:show,:destroy]
+  resources :posts, only: [:new,:create,:index,:show,:destroy] do
+    resource :post_comments, only: [:create,:destroy]
+  end
   resources :users, only: [:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
