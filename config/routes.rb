2bbe6e5e5dd96baa5e02d2a8a525a987ab3b49da
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-17 09:21:58 +0000

    [Add:] comment
    コメント削除機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   devise_for :users
   resources :posts, only: [:new,:create,:index,:show,:destroy] do
-    resource :post_comments, only: [:create,:destroy]
+    resources :post_comments, only: [:create,:destroy]
   end
   resources :users, only: [:show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
