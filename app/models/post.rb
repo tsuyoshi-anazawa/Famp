4717ff83c29404baa6512b32f13758ce181ac42b
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-19 01:33:29 +0000

    [Add:] favorite/model
    お気に入りモデルとルーティングを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post < ApplicationRecord
 
 	belongs_to :user
+	has_many :favorites, dependent: :destroy
 	has_many :post_comments, dependent: :destroy
+
+	def favorited_by?(user)
+		favorites.where(user_id:user.id).exists?
+	end
 
 end
