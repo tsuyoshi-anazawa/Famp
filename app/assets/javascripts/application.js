83695597aebe45877dd8ae504973d1f99bc8e5bd
Tsuyoshi Anazawa <anatsuyo0221@gmail.com>
2019-06-25 11:19:45 +0000

    [Add:] jquery
    ホーム画面、アバウト画面にjqueryでスライドショー、フェードインを実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,117 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
+
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+
+	$('.j-about').hide().fadeIn(3000);
+
+});
+
+$(function() {
+
+	$('#theTarget').skippr({
+		transition : 'slide',
+		speed : 1000,
+		easing : 'easeOutQuart',
+		navType : 'block',
+		childrenElementType : 'div',
+		arrows : false,
+		autoPlay : true,
+		autoPlayDuration : 3000,
+		keyboardOnAlways: true,
+		hidePrevious : false
+	});
+});
+
+$(function() {
+
+	$('.nav-text1').mouseover(function() {
+		$('.nav-text1').css({'color' : '#808080'});
+	});
+
+	$('.nav-text1').mouseout(function() {
+		$('.nav-text1').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text2').mouseover(function() {
+		$('.nav-text2').css({'color' : '#808080'});
+	});
+
+	$('.nav-text2').mouseout(function() {
+		$('.nav-text2').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text3').mouseover(function() {
+		$('.nav-text3').css({'color' : '#808080'});
+	});
+
+	$('.nav-text3').mouseout(function() {
+		$('.nav-text3').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text4').mouseover(function() {
+		$('.nav-text4').css({'color' : '#808080'});
+	});
+
+	$('.nav-text4').mouseout(function() {
+		$('.nav-text4').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text5').mouseover(function() {
+		$('.nav-text5').css({'color' : '#808080'});
+	});
+
+	$('.nav-text5').mouseout(function() {
+		$('.nav-text5').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text6').mouseover(function() {
+		$('.nav-text6').css({'color' : '#808080'});
+	});
+
+	$('.nav-text6').mouseout(function() {
+		$('.nav-text6').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text7').mouseover(function() {
+		$('.nav-text7').css({'color' : '#808080'});
+	});
+
+	$('.nav-text7').mouseout(function() {
+		$('.nav-text7').css({'color' : '#FFFFFF'});
+	});
+});
+
+$(function() {
+
+	$('.nav-text8').mouseover(function() {
+		$('.nav-text8').css({'color' : '#808080'});
+	});
+
+	$('.nav-text8').mouseout(function() {
+		$('.nav-text8').css({'color' : '#FFFFFF'});
+	});
+});
